050f38d7f5b6284ded3a48e7e6f8df6a666e0abc
Marta <118316713+martapolii@users.noreply.github.com>
2024-11-16 11:13:51 -0500

    Update express.js to import modules

--- server/express.js
+++ server/express.js
@@ -1,5 +1,21 @@
-// Import + configure + export express so rest of app can use it 
-  import express from 'express' 
-  const app = express()
-  /*... configure express ... */ 
-  export default app
+// Import modules
+import express from 'express'
+import bodyParser from 'body-parser'
+import cookieParser from 'cookie-parser'
+import compress from 'compression'
+import cors from 'cors'
+import helmet from 'helmet'
+
+// Define express app
+const app = express()
+
+// Configure modules
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cookieParser())
+app.use(compress())
+app.use(helmet())
+app.use(cors())
+
+// Export express app
+export default app
