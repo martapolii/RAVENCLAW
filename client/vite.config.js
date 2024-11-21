4494f8bfa0b1958ad12aa5e2e97e3d95e2be1286
Marta <118316713+martapolii@users.noreply.github.com>
2024-11-21 18:05:46 -0500

    Trying to troubleshoot deployment to github pages (error: could not resolve entry module "index.html")

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,14 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 export default defineConfig({
   plugins: [react()],
-  root: './', //ensure root is set to the root of the project
+  root: "./", //ensure root is set to the root of the project
   build: {
-    outDir: 'dist', //specifies output directory (attempt to fix index.html not populating in dist folder)
+    outDir: "dist", //specifies output directory (attempt to fix index.html not populating in dist folder)
     manifest: true,
     rollupOptions: {
-      input: "./index.html", //entry file for the app
+        input: "./index.html", //entry file for the app
     },
   },
+base: "/RAVENCLAW/", // added for GitHub compatibility
 });
