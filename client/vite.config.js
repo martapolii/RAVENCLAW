adf501065c8d381c434dbe000d91836ed8700938
Marta <118316713+martapolii@users.noreply.github.com>
2024-11-21 18:21:08 -0500

    Changed base for deployment on github pages

---
+++ client/vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+
+export default defineConfig({
+  plugins: [react()],
+base: "/RAVENCLAW/", // added for GitHub compatibility - THIS NEEDS TO BE THE BASE FOR GIT HUB PAGES TO WORK
+  //base: "/", //added to fix index.html not populating in dist folder - THIS NEEDS TO BE BASE FOR LOCAL DEPLOYMENT
+  root: "./", //ensure root is set to the root of the project
+  build: {
+    outDir: "dist", //specifies output directory (attempt to fix index.html not populating in dist folder)
+    manifest: true,
+    rollupOptions: {
+        input: "./index.html", //entry file for the app
+    },
+  },
+});
