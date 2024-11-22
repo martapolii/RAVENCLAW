d5be404b6c07b9f1c07be1191522ad4975ab7596
Marta <118316713+martapolii@users.noreply.github.com>
2024-11-22 13:11:18 -0500

    updated vite.config.js to simplify base path, and explicitly state input file

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,17 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { resolve } from "path";
+import path from "path";
 
-export default defineConfig({
+export default defineConfig(({ mode }) => {
+  return {
   plugins: [react()],
-base: "/RAVENCLAW/", // added for GitHub compatibility - THIS NEEDS TO BE THE BASE FOR GIT HUB PAGES TO WORK
-  //base: "/", //added to fix index.html not populating in dist folder - THIS NEEDS TO BE BASE FOR LOCAL DEPLOYMENT
-  root: "./", //ensure root is set to the root of the project
+  base: "/RAVENCLAW/", // Use "/" for local dev and "/RAVENCLAW/" for GitHub Pages
+  root: "./", //ensure root is set to where index.html is located
   build: {
-    outDir: "dist", //specifies output directory (attempt to fix index.html not populating in dist folder)
+    outDir: "dist", //specifies output directory (attempt to fix index.html not populating in dist folder) - worked! 
     manifest: true,
     rollupOptions: {
-        input: "./index.html", //entry file for the app
+        input: path.resolve(__dirname, "index.html"), //explicitly define the entry file for the app
     },
-  },
+  },
+  };
 });
