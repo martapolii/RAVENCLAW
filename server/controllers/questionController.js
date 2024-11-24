bb04288f3d7232c003a10b183fb4dd49b138ec47
Marta <118316713+martapolii@users.noreply.github.com>
2024-11-24 14:57:12 -0500

    Added route logic to GET route in question controller

--- server/controllers/questionController.js
+++ server/controllers/questionController.js
@@ -2,6 +2,14 @@ const TriviaQuestion = require('../models/trivia_questions.model');
 
 // fetches all trivia questions
 const getQuestions = async (req, res) => {
+  try {
+    const triviaQuestion = await TriviaQuestion.find(); 
+    res.status(200).json(contact); 
+  } catch (error) {
+    res.status(500).json({
+      error: 'Could not retrieve trivia questions'
+    });
+  } 
 };
 
 // fetches a specific question by ID
