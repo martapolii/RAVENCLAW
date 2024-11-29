09ae0e4d294a38d48547436b1c7acbfb0edbf5a1
tannhun55 <thuntus@my.centennialcollege.ca>
2024-11-29 13:48:48 -0500

    Removed QuestionDetails component and update references

--- client/src/mainRouter.jsx
+++ client/src/mainRouter.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/navbar';
 import Home from './pages/home';
 import QuestionList from './components/QuestionList';
-import QuestionDetails from './components/QuestionDetails';
+
 import Login from './pages/login';
 import Register from './pages/register';
 import UserProfile from './pages/UserProfile';
@@ -17,7 +17,6 @@ const MainRouter = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/questions" element={<QuestionList />} />
-        <Route path="/question-details" element={<QuestionDetails />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/user-profile" element={<UserProfile />} />
