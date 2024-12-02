701fafcd119930be7e58e9666cb64ab9395da963
Marta <118316713+martapolii@users.noreply.github.com>
2024-12-01 22:17:07 -0500

    Used NavLink to highlight active link

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -1,25 +1,24 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom'; // navink styles the active link automatically!!
 import '../css/navbar.css';
-import logo from '../../public/assets/logo.png';
 
 const Navbar = () => {
   return (
     <nav>
       <div>
         {/* logo */}
-        <Link to="/">
-          <img src={logo} alt="logo" />
-        </Link>
+        <NavLink to="/">
+          <img src="/assets/logo.png" alt="logo" />
+        </NavLink>
       </div>
       <ul style={{ display: 'flex', listStyleType: 'none', gap: '20px' }}>
-        <li><Link to="/" style={{ color: 'black', textDecoration: 'none' }}>Home</Link></li>
-        <li><Link to="/login" style={{ color: 'black', textDecoration: 'none' }}>Login</Link></li>
-        <li><Link to="/register" style={{ color: 'black', textDecoration: 'none' }}>Register</Link></li>
-        <li><Link to="/user-profile" style={{ color: 'black', textDecoration: 'none' }}>User Profile</Link></li>
-        <li><Link to="/game-play" style={{ color: 'black', textDecoration: 'none' }}>Game Play</Link></li>
-        <li><Link to="/admin-questions" style={{ color: 'black', textDecoration: 'none' }}>Admin Questions</Link></li>
-        <li><Link to="/admin-users" style={{ color: 'black', textDecoration: 'none' }}>Edit User Profile</Link></li>
+        <li><NavLink to="/" exact="true" activeClassName="active" style={{ color: 'black', textDecoration: 'none' }}>Home</NavLink></li>
+        <li><NavLink to="/login" activeClassName="active" style={{ color: 'black', textDecoration: 'none' }}>Login</NavLink></li>
+        <li><NavLink to="/register" activeClassName="active" style={{ color: 'black', textDecoration: 'none' }}>Register</NavLink></li>
+        <li><NavLink to="/user-profile" activeClassName="active" style={{ color: 'black', textDecoration: 'none' }}>User Profile</NavLink></li>
+        <li><NavLink to="/game-play" activeClassName="active" style={{ color: 'black', textDecoration: 'none' }}>Game Play</NavLink></li>
+        <li><NavLink to="/admin-questions" activeClassName="active" style={{ color: 'black', textDecoration: 'none' }}>Admin Questions</NavLink></li>
+        <li><NavLink to="/admin-users" activeClassName="active" style={{ color: 'black', textDecoration: 'none' }}>Edit User Profile</NavLink></li>
       </ul>
     </nav>
   );
